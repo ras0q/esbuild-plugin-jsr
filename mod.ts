33d824eb352831896d1354b6380acd9bc20613f5
ras0q <ras0q@users.noreply.github.com>
2025-06-08 06:29:47 +0900

    impl esbuild-plugin-jsr

---
+++ mod.ts
@@ -0,0 +1,96 @@
+import type { OnLoadResult, OnResolveResult, Plugin } from "esbuild";
+import {
+  format as formatSemver,
+  maxSatisfying as maxSatisfyingSemver,
+  parse as parseSemver,
+  parseRange as parseSemverRange,
+} from "@std/semver";
+import { bundle } from "@deno/emit";
+
+export type JsrPluginOptions = {
+  namespace?: string;
+};
+
+export const esbuildJsrPlugin = (options?: JsrPluginOptions): Plugin => {
+  const namespace = options?.namespace ?? "jsr-loader";
+
+  return {
+    name: "jsr",
+    setup: (build) => {
+      build.onResolve({ filter: /^jsr:|^@/ }, async (args) => {
+        if (args.kind !== "import-statement") return null;
+        if (build.initialOptions.external?.includes(args.path)) return null;
+
+        const resolved = import.meta.resolve(args.path);
+        if (!resolved) return null;
+
+        const isHTTP = resolved.startsWith("http://") ||
+          resolved.startsWith("https://");
+        const isJSR = resolved.startsWith("jsr:");
+        if (!isHTTP && !isJSR) return null;
+
+        if (isHTTP) {
+          return {
+            external: false,
+            namespace,
+            path: resolved,
+          } satisfies OnResolveResult;
+        }
+
+        if (isJSR) {
+          const matches = resolved.match(
+            /^jsr:(@[^/]+)\/([^@]+)@([^/]+)(?:\/(.+))?$/,
+          );
+          if (!matches) throw `Invalid JSR import: ${args.path}`;
+
+          const [, namespace, name, version, path] = matches;
+          const jsrioBasePath = `https://jsr.io/${namespace}/${name}`;
+          const meta = await (await fetch(`${jsrioBasePath}/meta.json`))
+            .json() as { versions: Record<string, unknown> };
+          const resolvedSemver = maxSatisfyingSemver(
+            Object.keys(meta.versions).map(parseSemver),
+            parseSemverRange(version),
+          );
+          if (!resolvedSemver) {
+            throw `No matching version found for ${namespace}/${name}@${version}`;
+          }
+
+          const resolvedVersion = formatSemver(resolvedSemver);
+          const versionMeta = await (await fetch(
+            `${jsrioBasePath}/${resolvedVersion}_meta.json`,
+          )).json() as { exports: Record<string, string> };
+
+          const exportPath = path ? `./${path}` : ".";
+          const resolvedPath = versionMeta.exports[exportPath]?.replace(
+            /^\.\/?/,
+            "",
+          );
+          if (!resolvedPath) throw `Export not found: ${exportPath}`;
+
+          const resolvedUrl =
+            `${jsrioBasePath}/${resolvedVersion}/${resolvedPath}`;
+
+          return {
+            external: false,
+            namespace,
+            path: resolvedUrl,
+          } satisfies OnResolveResult;
+        }
+      });
+
+      build.onLoad(
+        { filter: /\.(ts|js)/, namespace },
+        async (args) => {
+          const { code } = await bundle(args.path);
+
+          return {
+            contents: code,
+            loader: args.path.endsWith(".ts") ? "ts" : "js",
+          } satisfies OnLoadResult;
+        },
+      );
+    },
+  };
+};
+
+export default esbuildJsrPlugin;
